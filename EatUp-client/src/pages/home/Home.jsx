fcf7c09b3a2398ed061e27c81c0e8d72c89ea517
Mubarak <adesinamubarak123@gmail.com>
2024-05-30 06:20:20 -1100

    create and import the special dishes comp

--- EatUp-client/src/pages/home/Home.jsx
+++ EatUp-client/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './components/Banner'
 import Categories from './components/Categories'
+import SpecialDishes from './components/SpecialDishes'
 
 export default function Home() {
   return (
     <>
       <Banner />
       <Categories />
+      <SpecialDishes />
     </>
   )
 }
