168668f5bb1297bbb885828e8ee60257e635a8d5
Mubarak <adesinamubarak123@gmail.com>
2024-05-30 06:31:37 -1100

    install and import react stick and react carousel

--- EatUp-client/src/pages/home/components/SpecialDishes.jsx
+++ EatUp-client/src/pages/home/components/SpecialDishes.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function SpecialDishes() {
   return (
-    <div>SpecialDishes</div>
+    <div className='section-container my-20'>
+        <div className='text-left'>
+            <p className='subtitle'>Special Dishes</p>
+            <h2 className='title md:w-[520px]'>Standout Dishes From Our Menu</h2>
+        </div>
+    </div>
   )
 }
