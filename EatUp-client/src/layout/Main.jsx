7551a9c412345f2f5430a0d06165eff1ecc277b1
Mubarak <adesinamubarak123@gmail.com>
2024-05-31 03:27:00 -1100

    create the footer comp and import to home page

--- EatUp-client/src/layout/Main.jsx
+++ EatUp-client/src/layout/Main.jsx
@@ -3,12 +3,14 @@ import { Outlet } from 'react-router-dom'
 import '../../src/App.css'
 
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export default function Main() {
   return (
     <div>
         <Navbar />
         <Outlet />
+        <Footer />
     </div>
   )
 }
